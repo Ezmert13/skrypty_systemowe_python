566accddfacb3063f9cbf2f41c58f1b11002b8ab
Ezmert <ezmert@protonmail.ch>
2025-05-12 13:20:25 +0000

    Script to check disk usage

--- disk_usage.py
+++ disk_usage.py
@@ -1,3 +1,4 @@
 import subprocess
 
-df_output = subprocess.run(["df", "-h"], stdout=subprocess.P
+df_output = subprocess.run(["df", "-h"], capture_output=True, text=True)
+print(f"Aktualne zuzycie dysku: {df_output.stdout.strip()}") 
